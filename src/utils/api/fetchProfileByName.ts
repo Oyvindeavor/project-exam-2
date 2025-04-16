fae08ab05527f5218406892eade813a5445b101b
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2025-04-17 01:28:49 +0200

    added optional params

--- src/utils/api/fetchProfileByName.ts
+++ src/utils/api/fetchProfileByName.ts
@@ -4,21 +4,42 @@ import type { NoroffApiError } from '@/types/NoroffApi/errorMessage'
 import type { ApiErrorResponse } from '@/types/MyApi/ApiErrorResponse'
 import type { ProfileSingleResponse } from '@/types/NoroffApi/response/profileResponse'
 
-export default async function fetchProfileByName(name: string) {
-  const response = await fetch(ENDPOINTS.getProfileByName(name), {
-    method: 'GET',
-    headers: await getAuthHeaders(),
-  })
+interface fetchProfileByNameOptions {
+  bookings?: boolean
+  venues?: boolean
+}
 
-  if (!response.ok) {
-    const errorResponse: NoroffApiError = await response.json()
-    const errorMessage: ApiErrorResponse = {
-      error: errorResponse.errors?.[0]?.message || 'Failed to fetch profile',
-    }
-    console.error('Error fetching profile:', errorResponse.errors)
-    return { error: errorMessage }
+export default async function fetchProfileByName(
+  name: string,
+  { bookings = false, venues = false }: fetchProfileByNameOptions = {
+    bookings: false,
+    venues: false,
   }
+) {
+  try {
+    const queryParams: URLSearchParams = new URLSearchParams()
+    if (bookings) queryParams.append('_bookings', 'true')
+    if (venues) queryParams.append('_venues', 'true')
 
-  const data: ProfileSingleResponse = await response.json()
-  return { profile: data.data, meta: data.meta }
+    const url = `${ENDPOINTS.getProfileByName(name)}?${queryParams.toString()}`
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: await getAuthHeaders(),
+    })
+
+    if (!response.ok) {
+      const errorResponse: NoroffApiError = await response.json()
+      const errorMessage: ApiErrorResponse = {
+        error: errorResponse.errors?.[0]?.message || 'Failed to fetch profile',
+      }
+      console.error('Error fetching profile:', errorResponse.errors)
+      return { error: errorMessage }
+    }
+
+    const data: ProfileSingleResponse = await response.json()
+    return { profile: data.data, meta: data.meta }
+  } catch (error) {
+    console.error('Unexpected error fetching profile:', error)
+    return { error: { error: 'An unexpected error occurred' } }
+  }
 }
