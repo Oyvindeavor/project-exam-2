68201a5bc6ffeef937adb1611e0fb63d5cfa46d3
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2025-04-16 19:52:39 +0200

    Update page.tsx

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,38 +1,7 @@
-import LogoutButton from '@/components/LogoutButton'
-import { fetchProfileData } from '@/utils/api/fetchProfileData'
-
 export default async function Profile() {
-  const { data, error } = await fetchProfileData()
-  console.log('Profile data:', data)
-
-  if (error || !data) {
-    return (
-      <div className='flex flex-col items-center justify-center h-full'>
-        <h1 className='text-4xl font-bold text-danger'>Error</h1>
-        <p className='mt-4 text-lg text-danger'>{error || 'Could not load profile data.'}</p>
-        <div className='mt-4'>
-          <LogoutButton />
-        </div>
-      </div>
-    )
-  }
   return (
     <div className='flex flex-col items-center justify-center h-full'>
       <h1 className='text-4xl font-bold'>Profile</h1>
-      <h2>{data.name}</h2>
-      <p>Email: {data.email}</p>
-      {data.bio && <p>Bio: {data.bio}</p>}
-      {data.avatar && (
-        <img src={data.avatar.url} alt={data.avatar.alt} className='rounded-full w-24 h-24' />
-      )}{' '}
-      <p className='mt-4 text-lg'>This is your profile page.</p>
-      <p className='mt-2 text-sm text-gray-500'>
-        You can view and edit your profile information here.
-      </p>
-      <p>{data.banner.url}</p>
-      <div className='mt-4'>
-        <LogoutButton />
-      </div>
     </div>
   )
 }
