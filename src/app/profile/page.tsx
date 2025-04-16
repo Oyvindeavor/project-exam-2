dd38cbb3e24c090acc0bc97a25c6a0eeb2dca73e
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2025-04-16 19:53:17 +0200

    Update page.tsx

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,9 @@
+import LogoutButton from '@/components/LogoutButton'
 export default async function Profile() {
   return (
     <div className='flex flex-col items-center justify-center h-full'>
       <h1 className='text-4xl font-bold'>Profile</h1>
+      <LogoutButton />
     </div>
   )
 }
