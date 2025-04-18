77ee043f544fba4acd588ae818f68de6482aca56
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2025-04-19 01:17:56 +0200

    Create fetchAllVenues.ts

---
+++ src/utils/api/venues/fetchAllVenues.ts
@@ -0,0 +1,54 @@
+import { getAuthHeaders } from '@/utils/getAuthHeaders'
+import { ENDPOINTS } from '@/utils/constants/apiConstants'
+import type { VenuesResponse } from '@/types/NoroffApi/response/venuesResponse'
+import type { NoroffApiError } from '@/types/NoroffApi/errorMessage'
+import type { ApiErrorResponse } from '@/types/MyApi/ApiErrorResponse'
+
+interface fetchAllVenuesOptions {
+  sort?: string
+  sortOrder?: 'asc' | 'desc'
+  limit?: number
+  page?: number
+  _owner?: boolean
+  _bookings?: boolean
+}
+
+export default async function fetchAllVenues({
+  sort = 'created',
+  sortOrder = 'desc',
+  limit = 10,
+  page = 1,
+  _owner = false,
+  _bookings = false,
+}: fetchAllVenuesOptions = {}) {
+  try {
+    const queryParams: URLSearchParams = new URLSearchParams()
+    queryParams.append('sort', sort)
+    queryParams.append('sortOrder', sortOrder)
+    queryParams.append('limit', limit.toString())
+    queryParams.append('page', page.toString())
+    if (_owner) queryParams.append('_owner', 'true')
+    if (_bookings) queryParams.append('_bookings', 'true')
+    const url = `${ENDPOINTS.getVenues}?${queryParams.toString()}`
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: await getAuthHeaders(),
+    })
+
+    if (!response.ok) {
+      const errorResponse: NoroffApiError = await response.json()
+      const errorMessage: ApiErrorResponse = {
+        error: errorResponse.errors?.[0]?.message || 'Failed to fetch venues',
+      }
+      console.error('Error fetching venues:', errorResponse.errors)
+      console.log('Error message:', errorMessage)
+      throw new Error(errorMessage.error)
+    }
+
+    const data: VenuesResponse = await response.json()
+    return { venues: data.data, meta: data.meta }
+  } catch (error) {
+    console.error('Error fetching venues:', error)
+    throw error
+  }
+}
