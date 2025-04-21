2341bbbc4dedcf5092b3ae9ce1c5bd7edb4e42b1
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2025-04-21 06:29:34 +0200

    created function to create and retrieve booking by id
    - Created function for retrieving booking by its id
    - Created function to create new booking along with form and server action

---
+++ src/components/CreateBookingForm/action.ts
@@ -0,0 +1,46 @@
+'use server'
+
+import createBooking from '@/utils/api/bookings/createBooking'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+
+type ActionResult = {
+  error?: string
+}
+
+export default async function createBookingFormAction(
+  prevState: ActionResult | undefined,
+  formData: FormData
+): Promise<ActionResult> {
+  const venueId = formData.get('venueId') as string | null
+  const dateFrom = formData.get('dateFrom') as string | null
+  const dateTo = formData.get('dateTo') as string | null
+  const guests = formData.get('guests') as string | null
+
+  if (!venueId || !dateFrom || !dateTo || !guests) {
+    return { error: 'All fields are required.' }
+  }
+  console.log('Creating booking with data:', {
+    venueId,
+    dateFrom,
+    dateTo,
+    guests,
+  })
+
+  const result = await createBooking({
+    dateFrom,
+    dateTo,
+    guests: parseInt(guests, 10),
+    venueId,
+  }).catch((error) => {
+    console.error('Error creating booking:', error)
+    return { error: 'Failed to create booking.' }
+  })
+
+  if ('error' in result) {
+    return { error: result.error }
+  }
+
+  console.log('Booking created successfully:', result)
+  revalidatePath('venue/[id]/')
+}
