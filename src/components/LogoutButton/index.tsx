09c8294ff4a564248462f95a48cf619d479a7468
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2025-05-01 17:44:09 +0200

    Update index.tsx

--- src/components/LogoutButton/index.tsx
+++ src/components/LogoutButton/index.tsx
@@ -1,9 +1,15 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
+import React from 'react'
+import { LogOut } from 'lucide-react'
+
 export default function LogoutButton() {
   const router = useRouter()
+  const [isLoading, setIsLoading] = React.useState(false)
+
   const handleLogout = async () => {
+    setIsLoading(true) // Set loading true
     try {
       const res = await fetch('/api/auth/logout', {
         method: 'DELETE',
@@ -11,17 +17,21 @@ export default function LogoutButton() {
 
       if (res.ok) {
         router.push('/auth/login')
+        router.refresh()
       } else {
         console.error('Logout failed')
+        setIsLoading(false)
       }
     } catch (err) {
       console.error('Logout error:', err)
+      setIsLoading(false)
     }
   }
 
   return (
-    <button className='btn btn-danger' onClick={handleLogout}>
-      Logout
+    <button className='btn btn-danger' onClick={handleLogout} disabled={isLoading}>
+      {isLoading ? 'Logging out...' : 'Logout'}
+      <LogOut className='ms-2' />
     </button>
   )
 }
